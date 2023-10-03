14018f5470c8ee15a7b9b9e540ac3532e2d26e8e
Alcione Francisco de Borba <alcionefranciscodeBorba@gmail.com>
2023-10-03 20:26:25 -0300

    primeiro comit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_gridview_builder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_gridview_builder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_gridview_builder/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
